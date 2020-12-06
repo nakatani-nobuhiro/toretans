78674438926000277aacb68770bd11a9e209cc8e
nakatani-nobuhiro <52753202+nakatani-nobuhiro@users.noreply.github.com>
2020-12-07 00:16:06 +0900

    Add files via upload

--- js/scenery/sceneryMaster.js
+++ js/scenery/sceneryMaster.js
@@ -5,6 +5,6 @@ export const sceneryMaster = [
     { "iconImageSrc": "img/sceneryIcon/rain.png", "builder": new scenery.RainSceneryBuilder() },
     { "iconImageSrc": "img/sceneryIcon/wave.png", "builder": new scenery.WaveSceneryBuilder() },
     { "iconImageSrc": "img/sceneryIcon/moon.png", "builder": new scenery.MoonnightSceneryBuilder() },
-    { "iconImageSrc": "img/sceneryIcon/fallingleaves.png", "builder": new scenery.FallingLeavesSceneryBuilder() },
+    { "iconImageSrc": "img/sceneryIcon/fallingLeaves.png", "builder": new scenery.FallingLeavesSceneryBuilder() },
     { "iconImageSrc": "img/sceneryIcon/snow.png", "builder": new scenery.SnowSceneryBuilder() }
 ];
